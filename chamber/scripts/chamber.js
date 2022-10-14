76987af0d23c2be70040e5ae77117bedd73ea054
vallabisa <u068354659@byui.edu>
2022-10-14 17:37:31 +0800

    update js for day "come join . . . "

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,6 +9,8 @@ const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
 
+
+
 // select the elements to manipulate (output to)
 const datefield = document.querySelector(".date");
 const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
@@ -26,3 +28,11 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 // datefield.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
+// display a banner on Mondays or Tuesdays only
+var currentDay = now.getDay()
+if (currentDay = 2) {
+    document.getElementById("day").innerHTML = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+if (currentDay = 3) {
+    document.getElementById("day").innerHTML = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
