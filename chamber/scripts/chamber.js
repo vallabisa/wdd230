6bae6527b34b1960ca9b167b6656419b37ba813d
vallabisa <u068354659@byui.edu>
2022-11-02 17:24:37 +0800

    discover page added

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -36,3 +36,32 @@ if (currentDay = 2) {
 if (currentDay = 3) {
     document.getElementById("day").innerHTML = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
+
+
+
+// Progressive loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src')
+    img.className = "in"
+  };
+};
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
