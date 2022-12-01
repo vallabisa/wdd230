046bfc0bdab759ae8cffb7114417c2694ffb1aba
vallabisa <u068354659@byui.edu>
2022-12-01 16:47:26 +0800

    chamber discover page

---
+++ chamber/scripts/lazyload.js
@@ -0,0 +1,27 @@
+
+// Progressive loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src')
+    img.className = "in"
+  };
+};
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
